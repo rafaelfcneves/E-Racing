59e7ae6e2b4dc4eae0ce34f6341002650bd0cbf0
rafaelfcneves <rafaelfcneves@gmail.com>
2026-06-02 16:37:06 -0300

    Código base para abrir janela com a webcam

---
+++ OnboardingDV2026/teste.py
@@ -0,0 +1,26 @@
+import cv2
+
+def main():
+    # Chamada da WebCam
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        raise RuntimeError("A camera nao pode ser aberta.")
+
+    # Loop principal da transmissão de vídeo.
+    while True:
+        # Captura o frame da transmissão
+        ok, frame = cap.read()
+        if not ok:
+            break
+
+        # Função que cria a janela onde será transmitido o vídeo.
+        cv2.imshow("Teste de camera", frame)
+
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
