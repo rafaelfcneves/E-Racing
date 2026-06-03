6a0fe84874115288d0b6694a76d7910388d716ea
rafaelfcneves <rafaelfcneves@gmail.com>
2026-06-03 16:52:07 -0300

    Tentando fazer o código para identificação de duas mãos com mediapipe

--- OnboardingDV2026/teste.py
+++ OnboardingDV2026/teste.py
@@ -1,31 +1,52 @@
 import cv2
+import mediapipe as mp
+import numpy as np
 
 def main():
+    mp_hands = mp.solutions.hands
+    mp_draw = mp.solutions.drawing_utils
+    
     # Chamada da WebCam
     cap = cv2.VideoCapture(0)
     if not cap.isOpened():
         raise RuntimeError("A camera nao pode ser aberta.")
 
+    # Evita vazamento de memória caso a aplicação feche
+    with mp_hands.Hands(
+        max_num_hands=2,
+        min_detection_confidence=0.7,
+        min_tracking_confidence=0.7,
+    ) as hands:
+
     # Loop principal da transmissão de vídeo.
-    while True:
-        # Captura o frame da transmissão
-        ok, frame = cap.read()
-        if not ok:
-            break
+        while True:
+            # Captura o frame da transmissão
+            ok, frame = cap.read()
+            if not ok:
+                break
 
-        # Espelha o frame
-        frame = cv2.flip(frame, 1)
+            # Espelha o frame
+            frame = cv2.flip(frame, 1)
 
-        # Aplica o filtro preto com contornos brancos
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(gray, 100, 200)
+            # Converte o frame de BGR para RGB
+            frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # Função que cria a janela onde será transmitido o vídeo.
-        cv2.imshow("Teste de camera", edges)
+            # Retorna um objeto que contém uma lista das mãos que estão na tela (máx 2),
+            # Uma tupla que contém as coordenadas dos Landmarks
+            dados = hands.process(frame_rgb)
 
-        # Quebra o loop da transmissão de vídeo ao apertar "q"
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+
+
+            # Aplica o filtro preto com contornos brancos
+            gray = cv2.cvtColor(frame_landmarks, cv2.COLOR_RGB2GRAY)
+            edges = cv2.Canny(gray, 100, 200)
+
+            # Função que cria a janela onde será transmitido o vídeo.
+            cv2.imshow("Teste de camera", edges)
+
+            # Quebra o loop da transmissão de vídeo ao apertar "q"
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
     # Libera a Webcam
     cap.release()
