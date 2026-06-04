3db300e43c65f33b705f0226ad4de3765b5779d3
rafaelfcneves <rafaelfcneves@gmail.com>
2026-06-04 12:29:33 -0300

    Código refatorado

--- OnboardingDV2026/teste.py
+++ OnboardingDV2026/teste.py
@@ -10,39 +10,62 @@ FONT_THICKNESS = 1
 COLOR = (39, 127, 255)
 
 def obter_coordenadas(frame, dados, mp_hands, mp_draw):
+    """
+    extrai as coordenadas do começo do dedo do meio (Landmark 9) e desenha os LandMarks.
+    """
     y_left, y_right, x_left, x_right = None, None, None, None
+    # Se não houver 
     if not dados.multi_hand_landmarks:
         return y_left, y_right, x_left, x_right
+    
     height, width, _ = frame.shape
     # Recebe as mãos que estão na tela e as desenha
-    for i in range(len(dados.multi_hand_landmarks)):
-        hand = dados.multi_hand_landmarks[i]
-        # Lista de tuplas com as coordenadas das landmarks
-        lm = hand.landmark
-        mp_draw.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS)
-        classificacao = dados.multi_handedness[i].classification[0]
-        lado = classificacao.label
+    for i, hand in enumerate(dados.multi_hand_landmarks):
+        lm = hand.landmark   # Lista de tuplas com as coordenadas das landmarks
+        mp_draw.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS) # Desenha as LandMarks
         
-        if lado == "Left":
-            y_left = int(lm[9].y * height)
-            x_left = int(lm[9].x * width)
-        elif lado == "Right":
-            y_right = int(lm[9].y * height)
-            x_right = int(lm[9].x * width)
+        classificacao = dados.multi_handedness[i].classification[0]
+        lado = classificacao.label # Variável que indica se é mão esquerda ou direita
+        
+        if lado == "Left": y_left, x_left = int(lm[9].y * height), int(lm[9].x * width)
+        
+        elif lado == "Right": y_right, x_right = int(lm[9].y * height), int(lm[9].x * width)
 
-            coordenadas_x = [landmark.x for landmark in lm]
-            coordenadas_y = [landmark.y for landmark in lm]
-            text_x = int(min(coordenadas_x) * width)    # Coordenada x do texto
-            text_y = int(min(coordenadas_y) * height) - MARGIN   # Coordenada y do texto
+        # Faz uma lista de pontos x e y dos LandMarks e encontra o min para o texto de classificação
+        coordenadas_x = [landmark.x for landmark in lm]
+        coordenadas_y = [landmark.y for landmark in lm]
+        text_x = int(min(coordenadas_x) * width)    # Coordenada x do texto
+        text_y = int(min(coordenadas_y) * height) - MARGIN   # Coordenada y do texto
 
-            cv2.putText(frame,lado,(text_x, text_y),cv2.FONT_HERSHEY_SIMPLEX, 0.8,COLOR,2, cv2.LINE_AA)
+        cv2.putText(frame,lado,(text_x, text_y),cv2.FONT_HERSHEY_SIMPLEX, 0.8,COLOR,2, cv2.LINE_AA)
+    
     return y_left, y_right, x_left, x_right
+
+def calcular_angulacao(y_left, y_right, x_left, x_right):
+    """
+    Calcula o ângulo entre as mãos e retorna uma string que diz o comando que tem que ser executado.
+    """
+    comando=""
+    if None in (y_left, y_right, x_left, x_right):
+        return
+    delta_y = y_right - y_left
+    delta_x = x_right - x_left
+    tetha = np.arctan2(delta_y, delta_x) # Calcula o ângulo entre as mãos
+    if tetha < 0 and -(math.pi/2) < tetha < -(math.pi/12):
+        comando = "TURN LEFT"
+    elif tetha > 0 and math.pi/12 < tetha < math.pi/2:
+        comando = "TURN RIGHT"
+    elif -(math.pi/12) < tetha < (math.pi/12):
+        comando = "AHEAD"
+    return comando
+
+def desenhar_comando(comando, frame):
+    h, width, _ = frame.shape
+    cv2.putText(frame,comando,(int(width/2) - 100, 50),cv2.FONT_HERSHEY_SIMPLEX,0.8,COLOR,2,cv2.LINE_AA)
+
 def main():
     mp_hands = mp.solutions.hands
     mp_draw = mp.solutions.drawing_utils
-
-    x_esquerda, x_direita, y_esquerda, y_direita = None, None, None, None
-    comando =""
 
     # Chamada da WebCam
     cap = cv2.VideoCapture(0)
@@ -72,87 +95,10 @@ def main():
             # Retorna um objeto que contém uma lista das mãos que estão na tela (máx 2),
             # Uma tupla que contém as coordenadas dos Landmarks
             dados = hands.process(frame_rgb)
+            y_left, y_right, x_left, x_right = obter_coordenadas(frame, dados, mp_hands, mp_draw)
 
-            # Se identificados os Landmarks:
-            if dados.multi_hand_landmarks:
-                # Recebe as mãos que estão na tela e as desenha
-                for i in range(len(dados.multi_hand_landmarks)):
-                    hand = dados.multi_hand_landmarks[i]
-                    # Lista de tuplas com as coordenadas das landmarks
-                    lm = hand.landmark
-                    mp_draw.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS)
-
-                    # Classificar a mão
-                    classificacao = dados.multi_handedness[i].classification[0]
-                    lado = classificacao.label  # Retorna strings "Left" ou "Right"
-
-                    # Escrver se é mão esquerda ou direita.
-                    height, width, _ = frame_rgb.shape
-
-                    if lado == "Left":
-                        y_esquerda = int(lm[9].y * height)
-                        x_esquerda = int(lm[9].x * width)
-                    elif lado == "Right":
-                        y_direita = int(lm[9].y * height)
-                        x_direita = int(lm[9].x * width)
-
-                    coordenadas_x = [landmark.x for landmark in lm]
-                    coordenadas_y = [landmark.y for landmark in lm]
-                    text_x = int(min(coordenadas_x) * width)    # Coordenada x do texto
-                    text_y = int(min(coordenadas_y) * height) - MARGIN   # Coordenada y do texto
-
-                    cv2.putText(
-                        frame,       # Desenhando na sua tela preta
-                        lado,           # O texto "Esquerda" ou "Direita"
-                        (text_x, text_y),  # A posição dinâmica que acompanha a mão
-                        cv2.FONT_HERSHEY_SIMPLEX, 
-                        0.8, 
-                        COLOR,       # A cor laranja
-                        2, 
-                        cv2.LINE_AA
-                    )
-                if y_esquerda is not None and y_direita is not None and x_esquerda is not None and x_direita is not None:
-                    delta_y = y_direita - y_esquerda
-                    delta_x = x_direita - x_esquerda
-                    tetha = np.arctan2(delta_y, delta_x)
-
-                    if (tetha < 0 and -(math.pi/2) < tetha < -(math.pi/12)):
-                        comando = "TURN LEFT"
-                        cv2.putText(
-                            frame,
-                            comando,           # O texto "Left" ou "Right"
-                            (int(width/2) - 100, 50),  # A posição dinâmica que acompanha a mão
-                            cv2.FONT_HERSHEY_SIMPLEX, 
-                            0.8, 
-                            COLOR,       # A cor laranja
-                            2, 
-                            cv2.LINE_AA
-                        )
-                    elif (tetha > 0 and math.pi/12 < tetha < math.pi/2):
-                        comando = "TURN RIGHT"
-                        cv2.putText(
-                            frame,
-                            comando,           # O texto "Left" ou "Right"
-                            (int(width/2) - 100, 50),  # A posição dinâmica que acompanha a mão
-                            cv2.FONT_HERSHEY_SIMPLEX, 
-                            0.8, 
-                            COLOR,       # A cor laranja
-                            2, 
-                            cv2.LINE_AA
-                        )
-                    else:
-                        comando = "AHEAD"
-                        cv2.putText(
-                            frame,
-                            comando,           # O texto "Left" ou "Right"
-                            (int(width/2) - 100, 50),  # A posição dinâmica que acompanha a mão
-                            cv2.FONT_HERSHEY_SIMPLEX, 
-                            0.8, 
-                            COLOR,       # A cor laranja
-                            2, 
-                            cv2.LINE_AA
-                        )
-
+            comando = calcular_angulacao(y_left, y_right, x_left, x_right)
+            desenhar_comando(comando,frame)
             # Função que cria a janela onde será transmitido o vídeo.
             cv2.imshow("Teste de camera", frame)
 
