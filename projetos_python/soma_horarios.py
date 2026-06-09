5b1d8aa6656d55136c77944f19b05ce43266f229
rafaelfcneves <rafaelfcneves@gmail.com>
2026-06-09 17:14:34 -0300

    Removendo rastreamento da pasta de codeforces

--- projetos_python/soma_horarios.py
+++
@@ -1,7 +0,0 @@
-tempo_estudado = 0
-hrs_min = input().split(':')
-hrs_min = [int(x) for x in hrs_min]
-horas = str(hrs_min[0])
-minutos = str(hrs_min[1])
-tempo_estudado = horas+":"+minutos
-print(tempo_estudado)
